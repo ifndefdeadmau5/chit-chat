d4c86ce4cce866001556cc20ea84c4faff673570
Trevor <trevorme91@gmail.com>
2019-07-14 21:01:00 +0900

    Add simple chat server

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket) {
+  // this one send message to all clients except sender
+  socket.broadcast.emit('hi');
+
+  socket.on('chat message', function(msg) {
+    console.log(msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});

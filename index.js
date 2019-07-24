b98430e9e09ad0cd85b0f77aeecc9434dfdcfad0
Trevor <trevorme91@gmail.com>
2019-07-24 22:38:47 +0900

    Fix

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-http.listen(3333, function(){
+http.listen(3333, function() {
   console.log('listening on *:3333');
 });
 
 // Chatroom
 var numUsers = 0;
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
+  socket.on('new message', data => {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', data);
-
+  });
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
+  socket.on('add user', username => {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
@@ -26,26 +26,26 @@ io.on('connection', (socket) => {
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
-      numUsers: numUsers
+      numUsers: numUsers,
     });
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsers: numUsers,
     });
   });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
     socket.broadcast.emit('typing', {
-      username: socket.username
+      username: socket.username,
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
     socket.broadcast.emit('stop typing', {
-      username: socket.username
+      username: socket.username,
     });
   });
 
@@ -57,7 +57,7 @@ io.on('connection', (socket) => {
       // echo globally that this client has left
       socket.broadcast.emit('user left', {
         username: socket.username,
-        numUsers: numUsers
+        numUsers: numUsers,
       });
     }
   });

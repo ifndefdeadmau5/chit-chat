fb054a6c067afb3ad2ea3b7104d3e898eede205c
박성민 <trevorme91@gmail.com>
2019-08-07 10:53:31 +0900

    Add heroku setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-http.listen(3333, function() {
+http.listen(process.env.PORT, function() {
   console.log('listening on *:3333');
 });
 
